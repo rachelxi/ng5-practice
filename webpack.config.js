7a59918db05060b785232391239febeba09d43bf
Rachel Xi <rachelxi@Rachels-MacBook-Pro.local>
2017-12-30 11:34:58 +1300

    split libs and minimize bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-var path = require('path');
-var _root = path.resolve(__dirname, '.');
+const path = require('path');
+const _root = path.resolve(__dirname, '.');
 
 function root(args){
   args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [_root].concat(args));
 }
 
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   devtool: 'inline-source-map',
 
   entry: {
-    'polyfills': './polyfills.ts',
-    'app': './main.ts'
+    'polyfills': './app/polyfills.ts',
+    'vendor': './app/vendor.ts',
+    'app': './app/main.ts'
   },
 
   output: {
@@ -74,11 +76,13 @@ module.exports = {
     ),
 
     new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor', 'polyfills']
+      name: 'vendor',
     }),
 
     new HtmlWebpackPlugin({
       template: './index.html'
-    })
+    }),
+
+    new UglifyJsPlugin()
   ]
 };
